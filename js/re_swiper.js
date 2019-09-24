994b1ca8fe8c80f0f9e2c895620996abf280bc6f
hyeonggeun2 <dhgudrms12@gmail.com>
2019-09-24 19:03:24 +0900

    190924 수정

--- js/re_swiper.js
+++ js/re_swiper.js
@@ -1,13 +1,41 @@
-var swiper = new Swiper('.swiper-container', {
+// 2번째 스크립트
+var Sswiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+});
+
+// 1번째 스크립트
+var swiper = new Swiper('.s-swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // init: false,
+  loop: true,
+  pagination: {
+    el: '.s-swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.s-swiper-button-next',
+    prevEl: '.s-swiper-button-prev',
+  },
+  breakpoints: {
+    // 640: {
+    //   slidesPerView: 2,
+    //   spaceBetween: 20,
+    // },
+    // 768: {
+    //   slidesPerView: 3,
+    //   spaceBetween: 30,
+    // },
+
+    1000: {
+      slidesPerView: 4,
+      spaceBetween: 10,      
+    },
+  }
 });
