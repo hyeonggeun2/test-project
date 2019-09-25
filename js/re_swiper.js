c6b372433fe9103c76517a4d78bbc66890362648
minzeekim92 <minzeekim92@gmail.com>
2019-09-25 10:06:17 +0900

    20190925

--- js/re_swiper.js
+++ js/re_swiper.js
@@ -1,38 +1,5 @@
-
 // 2번째 스크립트
 var Sswiper = new Swiper('.swiper-container', {
-
-  slidesPerView: 1,
-  spaceBetween: 10,
-  // init: false,
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-
-    800: {
-      slidesPerView: 4,
-      spaceBetween: 40,      
-    },
-  }
-});
-
-// 2번째 스크립트
-var Sswiper = new Swiper('.s-swiper-container', {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
@@ -41,7 +8,6 @@ var Sswiper = new Swiper('.s-swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 });
-
 // 1번째 스크립트
 var swiper = new Swiper('.s-swiper-container', {
   slidesPerView: 1,
@@ -65,7 +31,6 @@ var swiper = new Swiper('.s-swiper-container', {
     //   slidesPerView: 3,
     //   spaceBetween: 30,
     // },
-
     1000: {
       slidesPerView: 4,
       spaceBetween: 10,      
