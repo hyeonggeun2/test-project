3340f9dd0f4418a3d32d5f860f33a1e440ccb5ee
minzeekim92 <minzeekim92@gmail.com>
2019-09-26 23:30:15 +0900

    20190926

--- js/submenu.js
+++ js/submenu.js
@@ -1,7 +1,17 @@
-var item = $('.mainMenu .menu-item');
+// var item = $('.mainMenu .menu-item');
 
 // 메인 메뉴 제어를 위한 함수
-item.on('mouseover focusin', function(){
-  item.removeClass('.active')
-  $(this).addClass('.active');
-});
+// item.on('mouseover focusin', function(){
+//  item.removeClass('.active')
+//  $(this).addClass('.active');
+// });
+
+$(function(){
+	$("#allmenudown ul li a").click(function(){
+		$("#allmenudown .depth1").slideUp();
+		if(!$(this).next().is(":visible"))
+		{
+			$(this).next().slideDown();
+		}
+	})
+})
